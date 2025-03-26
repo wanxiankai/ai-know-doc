2e94cfc04d2fb1071ae16ff6e38a2910ad17bd89
wanxiankai <xiankai.wan@gmail.com>
2025-03-26 14:47:23 +0800

    feat: add shadcn lib and setup theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
 
 export const metadata: Metadata = {
@@ -12,10 +13,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
