32233ec6ee065dc98dbe8944dc1c5d616d8ebdad
wanxiankai <xiankai.wan@gmail.com>
2025-04-03 16:37:10 +0800

    feat: setup the prisma with database

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { createClient } from "@/auth/server"
+import { prisma } from "@/lib/prisma"
 import { handleError } from "@/lib/utils"
 
 export const loginAction = async (email: string, password: string) => {
@@ -41,7 +42,12 @@ export const signUpAction = async (email: string, password: string) => {
         const userId = data.user?.id
         if (!userId) throw new Error('Error signing up')
         // add user to database
-
+        await prisma.user.create({
+            data: {
+                id: userId,
+                email,
+            }
+        })
         return { errorMessage: null }
 
     } catch (error) {
---
+++ src/db/migrations/20250403083250_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" TEXT NOT NULL,
+    "text" TEXT,
+    "authorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Document" ADD CONSTRAINT "Document_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
