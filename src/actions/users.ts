68581bdb41d5b7d12f935872f4499c6170e1aa5b
wanxiankai <xiankai.wan@gmail.com>
2025-04-03 09:54:29 +0800

    feat: init the users actions

---
+++ src/actions/users.ts
@@ -0,0 +1,50 @@
+'use server'
+
+import { createClient } from "@/auth/server"
+import { handleError } from "@/lib/utils"
+
+export const loginAction = async (email: string, password: string) => {
+    try {
+        const { auth } = await createClient()
+        const { error } = await auth.signInWithPassword({
+            email,
+            password,
+        })
+        if (error) throw error
+        return { errorMessage: null }
+
+    } catch (error) {
+        return handleError(error)
+    }
+}
+
+export const logOutAction = async () => {
+    try {
+        const { auth } = await createClient()
+        const { error } = await auth.signOut()
+        if (error) throw error
+        return { errorMessage: null }
+
+    } catch (error) {
+        return handleError(error)
+    }
+}
+
+export const signUpAction = async (email: string, password: string) => {
+    try {
+        const { auth } = await createClient()
+        const { data, error } = await auth.signUp({
+            email,
+            password,
+        })
+        if (error) throw error
+        const userId = data.user?.id
+        if (!userId) throw new Error('Error signing up')
+        // add user to database
+
+        return { errorMessage: null }
+
+    } catch (error) {
+        return handleError(error)
+    }
+}
