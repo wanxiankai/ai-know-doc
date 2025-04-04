5c494ac3a4f0df193ee4a72da3fccc835a209754
wanxiankai <xiankai.wan@gmail.com>
2025-04-04 23:12:33 +0800

    feat: update the main page componet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { getUser } from "@/auth/server"
+import AskAIButton from "@/components/AskAIButton"
+import DocTextInput from "@/components/DocTextInput"
+import NewDocumentButton from "@/components/NewDocumentButton"
+import { prisma } from "@/lib/prisma"
 
-function HomePage() {
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+
+}
+
+async function HomePage({ searchParams }: Props) {
+  const docIdParams = (await searchParams).docId
+  const user = await getUser()
+
+  const docId = Array.isArray(docIdParams) ? docIdParams[0] : docIdParams || ""
+  const doc = await prisma.document.findUnique({
+    where: {
+      id: docId,
+      authorId: user?.id,
+    },
+  })
+
   return (
-    <div>Home'Page</div>
+    <div className=' flex h-full flex-col items-center gap-4'>
+      <div className='flex w-full max-w-4xl justify-end gap-2'>
+        <AskAIButton user={user} />
+        <NewDocumentButton user={user} />
+      </div>
+
+      <DocTextInput docId={docId} startingDocText={doc?.text || ""} />
+    </div>
   )
 }
 
