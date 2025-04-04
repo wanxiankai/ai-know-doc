6a4e4e6f90ab6991de4be065da8fdc5492f321bc
wanxiankai <xiankai.wan@gmail.com>
2025-04-04 23:12:22 +0800

    feat: init siderbar component

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,50 @@
+import { getUser } from "@/auth/server"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupLabel,
+} from "@/components/ui/sidebar"
+import { prisma } from "@/lib/prisma";
+import { Document } from "@prisma/client";
+import Link from "next/link";
+import SidebarGroupContent from "./SidebarGroupContent";
+
+async function AppSidebar() {
+    const user = await getUser();
+    let documents: Document[] = [];
+
+    if (user) {
+        documents = await prisma.document.findMany({
+            where: {
+                authorId: user.id,
+            },
+            orderBy: {
+                updatedAt: "desc",
+            },
+        });
+    }
+
+    return (
+        <Sidebar>
+            <SidebarContent className="custom-scrollbar">
+                <SidebarGroup>
+                    <SidebarGroupLabel className="my-2 text-lg">
+                        {user ? "Your Documents" :
+                            (
+                                <p>
+                                    <Link href='/login' className="underline">
+                                        Login
+                                    </Link>{" "}
+                                    to view your documents
+                                </p>
+                            )}
+                    </SidebarGroupLabel>
+                    {user && <SidebarGroupContent docs={documents} />}
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
+
+export default AppSidebar
