b0ec834645dad890163cab8bcb184c12d2981c31
wanxiankai <xiankai.wan@gmail.com>
2025-04-05 23:18:34 +0800

    feat: optimize the select doc button

--- src/components/SelectDocButton.tsx
+++ src/components/SelectDocButton.tsx
@@ -1,13 +1,56 @@
 'use client'
 
+import useDoc from "@/hooks/useDoc"
 import { Document } from "@prisma/client"
+import { useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
+import { SidebarMenuButton } from "./ui/sidebar"
+import Link from "next/link"
 
 type Props = {
   doc: Document
 }
 function SelectDocButton({ doc }: Props) {
+  const docId = useSearchParams().get('docId') || ""
+
+  const { docText: selectedDocText } = useDoc();
+  const [shouldUseGlobalDocText, setShouldUseGlobalDocText] = useState(false)
+  const [localDocText, setLocalDocText] = useState(doc.text)
+
+  useEffect(() => {
+    if (docId === doc.id) {
+      setShouldUseGlobalDocText(true)
+    } else {
+      setShouldUseGlobalDocText(false)
+    }
+  }, [docId, doc.id])
+
+  useEffect(() => {
+    if (shouldUseGlobalDocText) {
+      setLocalDocText(selectedDocText)
+    }
+  }, [selectedDocText, shouldUseGlobalDocText])
+
+  const blankDocText = "EMPTY DOC";
+  let docText = localDocText || blankDocText
+  if (shouldUseGlobalDocText) {
+    docText = selectedDocText || blankDocText
+  }
+
   return (
-    <div>SelectDocButton</div>
+    <SidebarMenuButton
+      asChild
+      className={`items-start gap-0 pr-12 ${docId === doc.id && "bg-sidebar-accent/50"}`}
+    >
+      <Link href={`/?docId=${doc.id}`} className="flex h-fit flex-col">
+        <p className="w-full overflow-hidden truncate text-ellipsis whitespace-nowrap">
+          {docText}
+        </p>
+        <p className="text-muted-foreground text-xs">
+          {doc.updatedAt.toLocaleDateString()}
+        </p>
+      </Link>
+    </SidebarMenuButton>
   )
 }
 
